aac6a0b4705201cbf00554b069479cf230971542
Nate Parish <nate.j.parish@gmail.com>
2023-12-15 14:00:56 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(800, 800), "Spreadsheet Program");
+	sf::Event e;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(e))
+		{
+			if (e.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+	}
+
+	return 0;
+
+}
